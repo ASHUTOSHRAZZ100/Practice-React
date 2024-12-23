75cd6e33e4c56930953547a7f5219269f663487b
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-12-23 12:28:53 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { FooterSection, HeroSection } from './components'
+
+function App() {
+
+  return (
+    <>
+      <HeroSection/>
+      <FooterSection/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import HeroSection from "./Header/HeroSection";
+import FooterSection from "./Footer/FooterSection";
+
+export { HeroSection, FooterSection };
