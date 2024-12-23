fa05c2f91ac72a0c9ca4fc95888e4cff774c1020
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-12-23 12:47:50 +0530

    add LoginSection to components export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import HeroSection from "./Header/HeroSection";
 import FooterSection from "./Footer/FooterSection";
+import LoginSection from "./Login/LoginSection";
 
-export { HeroSection, FooterSection };
+export { HeroSection, FooterSection, LoginSection };
